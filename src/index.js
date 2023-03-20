d8c5d34e950327c79f36a6ad29e51bd1ede4c693
danieljuma5 <jumadaniel275@gmail.com>
2023-03-20 23:36:31 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const myForm = document.querySelector('#create-task-form');
+const myInput = document.querySelector('#new-task-description');
+const myUl = document.querySelector('#tasks');
+const userList = document.querySelector('#list');
+const btn = document.querySelector('.btn')
+
+document.addEventListener('DOMContentLoaded', () => {
+myForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const li = document.createElement('li');
+  const btn1 = document.createElement('button')
+  btn1.addEventListener('click', handleClick)
+  btn1.textContent = 'x'
+li.appendChild(document.createTextNode(`${myInput.value}`));
+li.appendChild(btn1)
+myUl.appendChild(li);
+myForm.reset(); 
+})
+})
+
+function handleClick(e) {
+  e.target.parentNode.remove();
+
+}
